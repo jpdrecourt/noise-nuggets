ff8ec4eb3807eb4e4dc23c1f0dacf767624dcac3
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2016-11-09 16:59:47 +0000

    Square on line animation with 2D geometry

--- src/app.js
+++ src/app.js
@@ -4,36 +4,48 @@ let Animation = require('./animation');
 let scene = new THREE.Scene();
 // let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -1000, 1000 );
+camera.position.z = 100;
+// camera.position.x = 30;
+// camera.position.y = 30;
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Cube
-let geometry = new THREE.BoxGeometry(30, 30, 30);
-let material = new THREE.MeshLambertMaterial({ color: 0xffffff });
-let cube = new THREE.Mesh(geometry, material);
-// cube.position.z = -15;
-scene.add(cube);
-camera.position.z = 100;
-camera.position.x = 30;
-camera.position.y = 30;
+// Square
+let squareMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+let squareGeometry = new THREE.Geometry();
+squareGeometry.vertices.push(new THREE.Vector3(-15.0, 15.0, 0.0));
+squareGeometry.vertices.push(new THREE.Vector3(15.0, 15.0, 0.0));
+squareGeometry.vertices.push(new THREE.Vector3(15.0, -15.0, 0.0));
+squareGeometry.vertices.push(new THREE.Vector3(-15.0, -15.0, 0.0));
+squareGeometry.faces.push(new THREE.Face3(0, 2, 1));
+squareGeometry.faces.push(new THREE.Face3(0, 3, 2));
+let square = new THREE.Mesh(squareGeometry, squareMaterial);
+// square.position.set(0.0, 0.0, 0.0);
+scene.add(square);
 
-// Cylindre
-geometry = new THREE.CylinderGeometry(2, 2, window.innerWidth, 5);
-let cylinder = new THREE.Mesh(geometry, material);
-cylinder.rotation.z = Math.PI / 2;
-scene.add(cylinder);
+// Line
+let lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+let lineGeometry = new THREE.Geometry();
+lineGeometry.vertices.push(
+  new THREE.Vector3(-window.innerWidth/2, 0, 0),
+  new THREE.Vector3(window.innerWidth/2, 0, 0)
+);
+var line = new THREE.Line( lineGeometry, lineMaterial );
+scene.add( line );
 
-// LIGHTS
-
-let ambientLight = new THREE.AmbientLight(0x555555);
-scene.add(ambientLight);
-let light = new THREE.DirectionalLight(0xFFFFFF, 1);
-light.position.set(-800, 900, 300);
-scene.add(light);
+// // Cylindre
+// let geometry = new THREE.CylinderGeometry(2, 2, window.innerWidth, 5);
+// let cylinder = new THREE.Mesh(geometry, material);
+// cylinder.rotation.z = Math.PI / 2;
+// scene.add(cylinder);
 
 // Animation dynamics
 let update = (step) => {
+  square.position.x += 120 * step;
+  if (square.position.x > window.innerWidth/2) {
+    square.position.x = -square.position.x;
+  }
 };
 
 let render = (dt) => {
